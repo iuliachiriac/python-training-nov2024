d1ba0ef59ba6aa8a3760680e47afc5588f64b389
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-11-11 13:47:47 +0200

    Add examples

---
+++ examples/indentation.py
@@ -0,0 +1,9 @@
+"""Simple script showing indentation principles"""
+
+x = 10
+
+if x > 0:
+    print("yay")
+    print("still inside if")
+
+print("outside if")
--- examples/script.py
+++ examples/script.py
@@ -1,1 +1,12 @@
-print("hello world!")
+print("/Users/iulia/PycharmProjects/python-training-nov2024/.venv/bin/python "
+      "/Users/iulia/PycharmProjects/python-training-nov2024/examples/script.py")
+
+x = 100 + \
+    7 + \
+    35
+print(x)
+
+x = (100 +
+     7 +
+     35)
+print(x)
